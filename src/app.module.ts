1867ff4b73647e3950e90eda6557dd71c55abab4
Jinwon-Dev <yjw9424@naver.com>
2022-11-02 00:35:34 +0900

    typeORM 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MembersModule } from './members/members.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [MembersModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), MembersModule], // typeORM 모듈 추가
 })
 export class AppModule {}
