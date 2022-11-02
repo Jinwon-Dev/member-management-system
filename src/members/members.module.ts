eb0d749f55023146e8b1492ad6eae630406aeac6
Jinwon-Dev <yjw9424@naver.com>
2022-11-02 17:44:31 +0900

    repository 생성

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembersController } from './members.controller';
 import { MembersService } from './members.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemberRepository } from './member.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MemberRepository])],
   controllers: [MembersController],
   providers: [MembersService],
 })
