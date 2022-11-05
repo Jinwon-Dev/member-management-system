f00bdc288e53f2786cd70d57a7485b23abffd20b
Jinwon-Dev <yjw9424@naver.com>
2022-11-05 14:21:36 +0900

    module에 repository를 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { MembersModule } from './members/members.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), MembersModule], // typeORM 모듈 추가
+  imports: [TypeOrmModule.forRoot(typeormConfig), MembersModule, AuthModule], // typeORM 모듈 추가
 })
 export class AppModule {}
