6efd99d64e5f3799122613342c06cb7f33553b65
Jinwon-Dev <yjw9424@naver.com>
2022-11-05 19:16:55 +0900

    feat: 시스템에 대한 회원 가입 기능 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(UserRepository)
+    @InjectRepository(User)
     private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    // 회원 가입 기능
+    const { username, password } = authCredentialsDto;
+    const user = this.userRepository.create({ username, password });
+
+    await this.userRepository.save(user);
+  }
 }
