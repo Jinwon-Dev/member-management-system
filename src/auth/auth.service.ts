90a023cd22f4977a5d16322042788e668c235913
Jinwon-Dev <yjw9424@naver.com>
2022-11-06 15:58:01 +0900

    feat: 사용자 이름 유니크화 및 중복일 때 예외 생성 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
@@ -16,6 +20,15 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
 
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === `23505`) {
+        // 사용자 이름이 중복일 시 예외 생성
+        throw new ConflictException(`Existing username`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique([`username`]) // 사용자 이름 유니크화(중복 X)
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
